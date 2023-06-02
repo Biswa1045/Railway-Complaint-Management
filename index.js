6ce0fd09b31c72806d017739c10bbde83b811b93
biswa1045 <biswajitsahu1045@gmail.com>
2023-06-02 13:07:29 +0530

    database connection com

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("lets start guys");
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    database: 'complaint_management',
+    port: '3306' // optional, default is 3306
+  });
+  connection.query('SELECT * FROM division', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return;
+    }
+    console.log('Query results:', results);
+  });
+  connection.end((err) => {
+    if (err) {
+      console.error('Error closing connection:', err);
+      return;
+    }
+    console.log('Connection closed.');
+  });
+  
+  
+  
