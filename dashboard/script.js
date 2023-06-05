023707e2b3227814df571655ef2a94c59439cea7
Sidhartha220 <91303514+Sidhartha220@users.noreply.github.com>
2023-06-05 12:23:16 +0530

    Add files via upload

---
+++ dashboard/script.js
@@ -0,0 +1,13 @@
+const allDropdown = document.querySelectorAll('.sidebar .side-dropdown');
+
+allDropdown.forEach(item=> {
+    const a = item.parentElement.querySelector('a:first-child');
+    a.addEventListener('click',function(e){
+        e.preventDefault();
+
+        
+        this.classList.toggle('active');
+        item.classList.toggle('show');
+
+    })
+})
